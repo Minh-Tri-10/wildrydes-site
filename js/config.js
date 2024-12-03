2eb154ca55e2dc288c5c32e27e44e4e844483053
Võ Trần Minh Trí <trivtmce182301@fpt.edu.vn>
2024-12-03 16:53:36 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_Nv6aNlTMb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1jj043690k6c26dmaf87j22ssg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_Jw9QlKk4g', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7tssqm5nqu0m9edckicnbn00of', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
